87573539ba0eae2b5a8a3d8fb49196ec2b714366
Edoardo Maria Liotta <edoardomaria.liotta@gmail.com>
2024-11-14 09:56:32 +0100

    Separate App (routing) and AppView

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './App.css'
-import Playground from './components/Playground/Playground'
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
-import { useEffect, useRef, useState } from 'react'
-import ServiceApi from './api/service-api'
+import React, { useState } from 'react'
 import Configuration from './components/Configuration/Configuration'
 import { ConfigurationType, getRoundId } from './api/config-api'
-import React from 'react'
-import { RoundInfo } from './domain/round-info'
-
-const serviceApi = new ServiceApi()
+import { Host, Present } from './AppView'
 
 const Home = ({
   onSave,
@@ -51,118 +46,6 @@ const App = () => {
       </Routes>
     </Router>
   )
-}
-
-const AppView = ({
-  hostView,
-  roundId: initialRoundId,
-}: {
-  hostView: boolean
-  roundId: string
-}) => {
-  const [roundInfo, setRoundInfo] = useState<RoundInfo>({
-    players: [],
-    questionItem: { id: '', isRevealed: false, text: '' },
-    items: [],
-  })
-  const [roundId, setRoundId] = useState<string>(initialRoundId)
-  const [roundHash, setRoundHash] = useState<string>('')
-  const [fetching, setFetching] = useState<boolean>(false)
-  const [socketConnection, setSocketConnection] = useState<
-    WebSocket | undefined
-  >()
-  const playground = useRef<Playground>(null)
-
-  const updateRoundId = (newRoundId: string) => {
-    setRoundId(newRoundId)
-    setFetching(true)
-    if (hostView) {
-      serviceApi.forceRefresh(newRoundId)
-    }
-  }
-  useEffect(() => {
-    console.log(roundInfo)
-    if (hostView && !fetching) {
-      setFetching(true)
-    }
-    if (!hostView) {
-      setSocketConnection(serviceApi.createSocketConnection())
-    }
-  }, [])
-
-  useEffect(() => {
-    if (fetching) {
-      setFetching(false)
-      serviceApi.getRound(roundId).then(j => setRoundInfo(j))
-    }
-  }, [fetching])
-
-  useEffect(() => {
-    console.log(roundInfo)
-    console.log(roundInfo.items)
-    setRoundHash(JSON.stringify(roundInfo))
-  }, [roundInfo])
-
-  useEffect(() => {
-    if (socketConnection !== undefined) {
-      socketConnection.onopen = function (_) {
-        setFetching(true)
-      }
-      socketConnection.onmessage = function (event) {
-        console.log(event)
-        const message = event.data
-        if (message && message.startsWith('need-update ')) {
-          updateRoundId(message.slice(12))
-        }
-        if ('show-strike' === message) {
-          playground.current?.showStrike()
-        }
-        if (message && message.startsWith('set-revealed ')) {
-          const split = message.split(' ')
-          playground.current?.animateRevealed(split[1], split[2] === 'true')
-        }
-        if (message && message.startsWith('set-player ')) {
-          playground.current?.setActivePlayer(message.slice(11))
-        }
-        if (message && message.startsWith('set-score-addition-mode ')) {
-          const newMode = message.slice(24)
-          console.log('Setting score addition mode to ' + newMode)
-          playground.current?.setScoreAdditionMode(newMode)
-        }
-      }
-      socketConnection.onclose = function (event) {
-        console.log(event)
-        setSocketConnection(undefined)
-      }
-      socketConnection.onerror = function (event) {
-        console.log(event)
-        setSocketConnection(undefined)
-      }
-    }
-  }, [socketConnection])
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Playground
-          key={`${roundId} ${roundHash}`}
-          ref={playground}
-          hostView={hostView}
-          roundId={roundId}
-          roundInfo={roundInfo}
-          updateRoundId={updateRoundId}
-        />
-      </header>
-    </div>
-  )
-}
-
-function Host({ roundId }: { roundId: string }) {
-  return AppView({ hostView: true, roundId })
-}
-
-function Present({ roundId }: { roundId: string }) {
-  return AppView({ hostView: false, roundId })
 }
 
 export default App
---
+++ src/AppView.tsx
@@ -0,0 +1,115 @@
+import ServiceApi from './api/service-api'
+import React, { useEffect, useRef, useState } from 'react'
+import { RoundInfo } from './domain/round-info'
+import Playground from './components/Playground/Playground'
+
+const serviceApi = new ServiceApi()
+const AppView = ({
+  hostView,
+  roundId: initialRoundId,
+}: {
+  hostView: boolean
+  roundId: string
+}) => {
+  const [roundInfo, setRoundInfo] = useState<RoundInfo>({
+    players: [],
+    questionItem: { id: '', isRevealed: false, text: '' },
+    items: [],
+  })
+  const [roundId, setRoundId] = useState<string>(initialRoundId)
+  const [roundHash, setRoundHash] = useState<string>('')
+  const [fetching, setFetching] = useState<boolean>(false)
+  const [socketConnection, setSocketConnection] = useState<
+    WebSocket | undefined
+  >()
+  const playground = useRef<Playground>(null)
+
+  const updateRoundId = (newRoundId: string) => {
+    setRoundId(newRoundId)
+    setFetching(true)
+    if (hostView) {
+      serviceApi.forceRefresh(newRoundId)
+    }
+  }
+  useEffect(() => {
+    console.log(roundInfo)
+    if (hostView && !fetching) {
+      setFetching(true)
+    }
+    if (!hostView) {
+      setSocketConnection(serviceApi.createSocketConnection())
+    }
+  }, [])
+
+  useEffect(() => {
+    if (fetching) {
+      setFetching(false)
+      serviceApi.getRound(roundId).then(j => setRoundInfo(j))
+    }
+  }, [fetching])
+
+  useEffect(() => {
+    console.log(roundInfo)
+    console.log(roundInfo.items)
+    setRoundHash(JSON.stringify(roundInfo))
+  }, [roundInfo])
+
+  useEffect(() => {
+    if (socketConnection !== undefined) {
+      socketConnection.onopen = function (_) {
+        setFetching(true)
+      }
+      socketConnection.onmessage = function (event) {
+        console.log(event)
+        const message = event.data
+        if (message && message.startsWith('need-update ')) {
+          updateRoundId(message.slice(12))
+        }
+        if ('show-strike' === message) {
+          playground.current?.showStrike()
+        }
+        if (message && message.startsWith('set-revealed ')) {
+          const split = message.split(' ')
+          playground.current?.animateRevealed(split[1], split[2] === 'true')
+        }
+        if (message && message.startsWith('set-player ')) {
+          playground.current?.setActivePlayer(message.slice(11))
+        }
+        if (message && message.startsWith('set-score-addition-mode ')) {
+          const newMode = message.slice(24)
+          console.log('Setting score addition mode to ' + newMode)
+          playground.current?.setScoreAdditionMode(newMode)
+        }
+      }
+      socketConnection.onclose = function (event) {
+        console.log(event)
+        setSocketConnection(undefined)
+      }
+      socketConnection.onerror = function (event) {
+        console.log(event)
+        setSocketConnection(undefined)
+      }
+    }
+  }, [socketConnection])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Playground
+          key={`${roundId} ${roundHash}`}
+          ref={playground}
+          hostView={hostView}
+          roundId={roundId}
+          roundInfo={roundInfo}
+          updateRoundId={updateRoundId}
+        />
+      </header>
+    </div>
+  )
+}
+
+export const Host = ({ roundId }: { roundId: string }) =>
+  AppView({ hostView: true, roundId })
+
+export const Present = ({ roundId }: { roundId: string }) =>
+  AppView({ hostView: false, roundId })
